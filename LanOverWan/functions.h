9e1e799c55729b934cf00cffd7dbe100d6b28a8c
roki5829 <rokas.kilinskas@mif.stud.vu.lt>
2020-01-24 10:53:23 +0200

    dns-sd browsing

--- LanOverWan/functions.h
+++ LanOverWan/functions.h
@@ -8,6 +8,10 @@
 #include <mswsock.h>
 #include <iostream>
 #include <detours.h>
+#include <string>
+#include <iomanip>
+#include <intrin.h>
+using namespace std;
 
 typedef int(WSAAPI* connectFunction)(SOCKET socket, const sockaddr* name, int namelen);
 typedef int(WSAAPI* WSAConnectFunction)(SOCKET socket, const sockaddr* name, int namelen, LPWSABUF lpCallerData, LPWSABUF lpCalleeData, LPQOS lpSQOS, LPQOS lpGQOS);
@@ -49,8 +53,9 @@ typedef BOOL(WSAAPI* WSAConnectByNameWFunction)(SOCKET s, LPWSTR nodename, LPWST
 typedef BOOL(WSAAPI* WSAConnectByNameAFunction)(SOCKET s, LPCSTR nodename, LPCSTR servicename, LPDWORD LocalAddressLength, LPSOCKADDR LocalAddress, LPDWORD RemoteAddressLength, LPSOCKADDR RemoteAddress, const timeval* timeout, LPWSAOVERLAPPED Reserved);
 
 typedef BOOL(WSAAPI* WSAConnectByListFunction)(SOCKET s, PSOCKET_ADDRESS_LIST SocketAddress, LPDWORD LocalAddressLength, LPSOCKADDR LocalAddress, LPDWORD RemoteAddressLength, LPSOCKADDR RemoteAddress, const timeval* timeout, LPWSAOVERLAPPED Reserved);
-
-
+typedef int (WSAAPI* sendFunction)(SOCKET s, const char* buf, int len, int flags);
+typedef int (WSAAPI* recvFunction)(SOCKET s, char* buf, int len, int flags);
+typedef BOOL (WINAPI* IsDebuggerPresentFunction)();
 extern connectFunction connectOriginal;
 extern WSAConnectFunction WSAConnectOriginal;
 extern ioctlsocketFunction ioctlsocketOriginal;
@@ -77,3 +82,49 @@ extern WSAConnectByNameAFunction WSAConnectByNameAOriginal;
 extern WSAConnectByListFunction WSAConnectByListOriginal;
 extern LPFN_CONNECTEX ConnectExOriginal;
 extern LPFN_CONNECTEX ConnectExReturned;
+extern sendFunction sendOriginal;
+extern recvFunction recvOriginal;
+extern IsDebuggerPresentFunction IsDebuggerPresentOriginal;
+void WSAAPI cleanup();
+int WSAAPI connectReplaced(SOCKET socket, const sockaddr* name, int namelen);
+int WSAAPI WSAConnectReplaced(SOCKET socket, const sockaddr* name, int namelen, LPWSABUF lpCallerData, LPWSABUF lpCalleeData, LPQOS lpSQOS, LPQOS lpGQOS);
+int WSAAPI ioctlsocketReplaced(SOCKET s, long cmd, u_long* argp);
+int WSAAPI WSAIoctlReplaced(SOCKET s, DWORD dwIoControlCode, LPVOID lpvInBuffer, DWORD cbInBuffer, LPVOID lpvOutBuffer, DWORD cbOutBuffer, LPDWORD lpcbBytesReturned, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine);
+int WSAAPI WSAAsyncSelectReplaced(SOCKET s, HWND hWnd, u_int wMsg, long lEvent);
+int WSAAPI WSAEventSelectReplaced(SOCKET s, WSAEVENT hEventObject, long lNetworkEvents);
+int WSAAPI sendtoReplaced(SOCKET s, const char* buf, int len, int flags, const sockaddr* to, int tolen);
+int WSAAPI recvfromReplaced(SOCKET s, char* buf, int len, int flags, sockaddr* from, int* fromlen);
+int WSAAPI WSASendToReplaced(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesSent, DWORD dwFlags, const sockaddr* lpTo, int iTolen, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine);
+int WSAAPI WSARecvFromReplaced(SOCKET s, LPWSABUF lpBuffers, DWORD dwBufferCount, LPDWORD lpNumberOfBytesRecvd, LPDWORD lpFlags, sockaddr* lpFrom, LPINT lpFromlen, LPWSAOVERLAPPED lpOverlapped, LPWSAOVERLAPPED_COMPLETION_ROUTINE lpCompletionRoutine);
+BOOL WSAAPI WSAGetOverlappedResultReplaced(SOCKET s, LPWSAOVERLAPPED lpOverlapped, LPDWORD lpcbTransfer, BOOL fWait, LPDWORD lpdwFlags);
+DWORD WSAAPI WSAWaitForMultipleEventsReplaced(DWORD cEvents, const WSAEVENT* lphEvents, BOOL fWaitAll, DWORD dwTimeout, BOOL fAlertable);
+BOOL WSAAPI GetOverlappedResultReplaced(HANDLE hFile, LPOVERLAPPED lpOverlapped, LPDWORD lpNumberOfBytesTransferred, BOOL bWait);
+BOOL WSAAPI GetOverlappedResultExReplaced(HANDLE hFile, LPOVERLAPPED lpOverlapped, LPDWORD lpNumberOfBytesTransferred, DWORD dwMilliseconds, BOOL bAlertable);
+BOOL WSAAPI GetQueuedCompletionStatusReplaced(
+	HANDLE       CompletionPort,
+	LPDWORD      lpNumberOfBytesTransferred,
+	PULONG_PTR   lpCompletionKey,
+	LPOVERLAPPED* lpOverlapped,
+	DWORD        dwMilliseconds
+);
+BOOL WSAAPI GetQueuedCompletionStatusExReplaced(
+	HANDLE             CompletionPort,
+	LPOVERLAPPED_ENTRY lpCompletionPortEntries,
+	ULONG              ulCount,
+	PULONG             ulNumEntriesRemoved,
+	DWORD              dwMilliseconds,
+	BOOL               fAlertable
+);
+LRESULT WSAAPI DispatchMessageReplaced(const MSG* lpMsg);
+HANDLE WSAAPI CreateIoCompletionPortReplaced(HANDLE FileHandle, HANDLE ExistingCompletionPort, ULONG_PTR CompletionKey, DWORD NumberOfConcurrentThreads);
+int WSAAPI closesocketReplaced(SOCKET s);
+BOOL WSAAPI FreeLibraryReplaced(HMODULE hLibModule);
+BOOL WSAAPI CloseHandleReplaced(HANDLE hObject);
+
+BOOL WSAAPI WSAConnectByNameWReplaced(SOCKET s, LPWSTR nodename, LPWSTR servicename, LPDWORD LocalAddressLength, LPSOCKADDR LocalAddress, LPDWORD RemoteAddressLength, LPSOCKADDR RemoteAddress, const timeval* timeout, LPWSAOVERLAPPED Reserved);
+BOOL WSAAPI WSAConnectByNameAReplaced(SOCKET s, LPCSTR nodename, LPCSTR servicename, LPDWORD LocalAddressLength, LPSOCKADDR LocalAddress, LPDWORD RemoteAddressLength, LPSOCKADDR RemoteAddress, const timeval* timeout, LPWSAOVERLAPPED Reserved);
+BOOL WSAAPI WSAConnectByListReplaced(SOCKET s, PSOCKET_ADDRESS_LIST SocketAddress, LPDWORD LocalAddressLength, LPSOCKADDR LocalAddress, LPDWORD RemoteAddressLength, LPSOCKADDR RemoteAddress, const timeval* timeout, LPWSAOVERLAPPED Reserved);
+BOOL WSAAPI ConnectExReplaced(SOCKET s, const sockaddr* name, int namelen, PVOID lpSendBuffer, DWORD dwSendDataLength, LPDWORD lpdwBytesSent, LPOVERLAPPED lpOverlapped);
+int WSAAPI sendReplaced(SOCKET s, const char* buf, int len, int flags);
+int WSAAPI recvReplaced(SOCKET s, char* buf, int len, int flags);
+BOOL WINAPI IsDebuggerPresentReplaced();
--- LanOverWan/main.cpp
+++ LanOverWan/main.cpp
@@ -1,12 +1,18 @@
-
-#include <windows.h>
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <detours.h>
+#include <fstream>
 #include "functions.h"
+#include <dns_sd.h>
+#include <thread>
 using namespace std;
 
 bool initiateFunctions();
+DNSServiceRef client;
+static void DNSSD_API browse_reply(DNSServiceRef sdref, const DNSServiceFlags flags, uint32_t ifIndex, DNSServiceErrorType errorCode,
+	const char* replyName, const char* replyType, const char* replyDomain, void* context);
 
+void browseTask();
+thread browseThread;
 BOOL APIENTRY DllMain(HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	//
@@ -19,7 +25,12 @@ BOOL APIENTRY DllMain(HANDLE hModule, DWORD  ul_reason_for_call, LPVOID lpReserv
 	case DLL_PROCESS_ATTACH:
 	{
 		AllocConsole();
+		freopen("CONOUT$", "w", stdout);
 		cout << "DLL_PROCESS_ATTACH" << endl;
+		cout << hModule << endl;
+		cout << DNSServiceGetProperty << endl;
+		//initiateFunctions();
+		browseThread = thread (browseTask);
 	}
 
 	break;
@@ -44,11 +55,13 @@ bool initiateFunctions() {
 
 	connectOriginal = connect;
 	WSAConnectOriginal = WSAConnect;
-
+	WSAIoctlOriginal = WSAIoctl;
 	sendtoOriginal = sendto;
 	recvfromOriginal = recvfrom;
 	WSASendToOriginal = WSASendTo;
 	WSARecvFromOriginal = WSARecvFrom;
+	sendOriginal = send;
+	recvOriginal = recv;
 	//WSAGetOverlappedResultOriginal = WSAGetOverlappedResult;
 
 
@@ -56,11 +69,17 @@ bool initiateFunctions() {
 	WSAConnectByNameWOriginal = WSAConnectByNameW;
 	WSAConnectByNameAOriginal = WSAConnectByNameA;
 	WSAConnectByListOriginal = WSAConnectByList;
-
+	IsDebuggerPresentOriginal = IsDebuggerPresent;
 	DetourAttach((PVOID*)(&connectOriginal), connectReplaced);
 	DetourAttach((PVOID*)(&WSAConnectOriginal), WSAConnectReplaced);
-
-
+	DetourAttach((PVOID*)(&WSAIoctlOriginal), WSAIoctlReplaced);
+	DetourAttach((PVOID*)(&sendtoOriginal), sendtoReplaced);
+	DetourAttach((PVOID*)(&recvfromOriginal), recvfromReplaced);
+	DetourAttach((PVOID*)(&WSASendToOriginal), WSASendToReplaced);
+	DetourAttach((PVOID*)(&WSARecvFromOriginal), WSARecvFromReplaced);
+	DetourAttach((PVOID*)(&sendOriginal), sendReplaced);
+	DetourAttach((PVOID*)(&recvOriginal), recvReplaced);
+	DetourAttach((PVOID*)(&IsDebuggerPresentOriginal), IsDebuggerPresentReplaced);
 
 	DetourAttach((PVOID*)(&WSAConnectByNameWOriginal), WSAConnectByNameWReplaced);
 	DetourAttach((PVOID*)(&WSAConnectByNameAOriginal), WSAConnectByNameAReplaced);
@@ -74,4 +93,33 @@ bool initiateFunctions() {
 	DetourTransactionCommit();
 	//new std::thread(ts3plugin_configure, (void*)NULL, (void*)NULL);
 	return true;
+}
+
+static void DNSSD_API browse_reply(DNSServiceRef sdref, const DNSServiceFlags flags, uint32_t ifIndex, DNSServiceErrorType errorCode,
+	const char* replyName, const char* replyType, const char* replyDomain, void* context) {
+	if (flags & kDNSServiceFlagsAdd) {
+
+		cout << "Added game ";
+	}
+	else {
+		cout << "Removed game ";
+	}
+	cout << "(" << ifIndex << "): " << replyName << endl;
+}
+
+void browseTask()
+{
+	DNSServiceErrorType err = DNSServiceBrowse(&client, 0, kDNSServiceInterfaceIndexAny, "_blizzard._udp,_w3xp272f", "", browse_reply, NULL);
+	if (err == kDNSServiceErr_NoError) {
+		cout << "Service browser started successfully" << endl;
+	}
+	else {
+		cout << "Service browser failed " << err << endl;
+		return;
+	}
+	while (DNSServiceProcessResult(client) == kDNSServiceErr_NoError) {
+		cout << "Result recieved" << endl;
+	}
+	DNSServiceRefDeallocate(client);
+
 }
